272dcbac7cf26704920c0b480b5aaec961ae7be9
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2026-02-19 19:53:03 +0530

    feat: Update favicon to PNG and removed unwanted sentences from server startup log.

--- backendAPI/server.js
+++ backendAPI/server.js
@@ -55,6 +55,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`🚀 Timely API server running on port ${PORT}`);
+        console.log(`Timely API server running on port ${PORT}`);
     });
 });
